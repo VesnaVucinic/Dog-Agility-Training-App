b8fee90a3974378ba8653598e3db705460e1bddd
vesna.vucinic68@gmail.com <vesna@LAPTOP-KHUC3ESJ.localdomain>
2020-04-28 17:41:59 +0100

    add validations and new user

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,7 @@ class User < ApplicationRecord
   has_many :courses, through: :trainings
 
   has_secure_password
+  validates :username, :email, presence: true
+  validates :username, uniqueness: true
+
 end
