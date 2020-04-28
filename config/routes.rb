abbd3745746b58a6a1c3f4cee59774393ffbe217
vesna.vucinic68@gmail.com <vesna@LAPTOP-KHUC3ESJ.localdomain>
2020-04-28 14:13:31 +0100

    add logout routs and methods in users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  root "trainings#index"
+  root "sessions#index"
 
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
-  delete '/logout' => 'sessions#destroy'
+  
 
   get '/signup' => 'users#new'
   post '/signup' => 'users#create'
