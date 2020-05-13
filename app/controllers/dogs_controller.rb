7905858ff769f65299d88336761c7b0dad4adf45
vesna.vucinic68@gmail.com <vesna@LAPTOP-KHUC3ESJ.localdomain>
2020-05-13 16:54:32 +0100

    add actions in dog controller

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,27 @@
 class DogsController < ApplicationController
+    def index
+        @dogs = Dog.all
+    end
+
+    def new
+        @dog = Dog.new  
+    end
+
+    def create
+        @dog = current_user.dogs.build(dog_params)
+        if @dog.save
+          redirect_to dog_path(@dog)
+        else
+          render :new
+        end
+    end
+
+    private
+
+    def dog_params
+        params.require(:dog).permit(:name, :age, :breed, :category, :user_id)
+    end
+
+
+
 end
